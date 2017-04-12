ca2064b2ab1b686126fbe705fd0d8301d353c9c1
Frank Solleveld <fsolleveld@outlook.com>
2017-04-12 09:58:23 +0200

    + 2. Connecting UI to the Code

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Frank Solleveld on 11-04-17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //MARK:UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: Any) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+
+}
+
